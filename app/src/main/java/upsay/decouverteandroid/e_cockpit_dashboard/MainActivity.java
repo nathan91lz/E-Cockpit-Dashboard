200d3d781d7d68e69dd8df412ef5c6ead800b419
zaza <135619134+nathan91lz@users.noreply.github.com>
2024-09-11 16:00:38 +0200

    Bluetooth status indicator
    Used with a test button

--- app/src/main/java/upsay/decouverteandroid/e_cockpit_dashboard/MainActivity.java
+++ app/src/main/java/upsay/decouverteandroid/e_cockpit_dashboard/MainActivity.java
@@ -1,6 +1,10 @@
 package upsay.decouverteandroid.e_cockpit_dashboard;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +19,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    //  setup is set
+
+    // Initialize the ImageView and Button
+    ImageView imgBtIndicator = findViewById(R.id.imgBtIndicator);
+    Button circleButton = findViewById(R.id.bpTest);
+    TextView txtEtatBt = findViewById(R.id.txtEtatBt);
+
+    // device connected status
+    circleButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            imgBtIndicator.setImageResource(R.drawable.green_circle);
+            txtEtatBt.setText("Connected");
+        }
+    });
+
     }
-// test2
 }
