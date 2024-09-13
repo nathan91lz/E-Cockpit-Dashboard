9c43caaabac6f67050e972708bff9f759985d613
zaza <135619134+nathan91lz@users.noreply.github.com>
2024-09-13 11:07:28 +0200

    Bluetooth class setuped

--- app/src/main/java/upsay/decouverteandroid/e_cockpit_dashboard/Bluetooth.java
+++ app/src/main/java/upsay/decouverteandroid/e_cockpit_dashboard/Bluetooth.java
@@ -2,9 +2,24 @@ package upsay.decouverteandroid.e_cockpit_dashboard;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
 
 public class Bluetooth {
-    protected String bluetoothDevice;
+    private static String bluetoothDeviceName;
+    private static String bluetoothDeviceMacAddress;
+    private BluetoothSocket socket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+
+    // UUID for the serial port service on Bluetooth devices
+    private static final UUID UUID_SERIAL_PORT = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     // function to get the MAC address of the currently connected Bluetooth device
     public static String getConnectedDeviceMac() {
@@ -15,12 +30,12 @@ public class Bluetooth {
             return "Bluetooth not enabled or not supported";
         }
 
-        // Iterate over paired devices to find connected device
-        for (BluetoothDevice device : bluetoothAdapter.getBondedDevices()) {
-            // Add your logic here to determine if the device is connected
-            // In some cases, this can be done using Bluetooth profiles
-            // Example just returns the first paired device's MAC address
-            return device.getAddress();  // Return the MAC address
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        // iterate over paired devices to find connected device
+        for (BluetoothDevice device : pairedDevices) {
+            bluetoothDeviceMacAddress = device.getAddress();
+            return bluetoothDeviceMacAddress;
         }
 
         return "No connected Bluetooth device found";
@@ -35,13 +50,83 @@ public class Bluetooth {
             return "Bluetooth not enabled or not supported";
         }
 
-        // iterate over paired devices to find connected device
-        for (BluetoothDevice device : bluetoothAdapter.getBondedDevices()) {
-            // Return the name of the first paired device
-            return device.getName();
-        }
 
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        // iterate over paired devices to find connected device
+        for (BluetoothDevice device : pairedDevices) {
+            // Return the name of the first paired device
+            bluetoothDeviceName = device.getName();
+            return bluetoothDeviceName;
+        }
         return "No connected Bluetooth device found";
+    }
+
+
+    // connection to the Bluetooth device using MAC address
+    public void connect(String macAddress) throws IOException {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(macAddress);
+
+        socket = device.createRfcommSocketToServiceRecord(UUID_SERIAL_PORT);
+        bluetoothAdapter.cancelDiscovery(); // Stop discovery, improves connection time
+        socket.connect();
+
+        outputStream = socket.getOutputStream();
+        inputStream = socket.getInputStream();
+    }
+
+    // Send AT command to initialize the OBD-II connection
+    public void initializeConnection() throws IOException {
+        sendATCommand("ATZ\r");
+        sendATCommand("ATE0\r");
+        sendATCommand("ATL0\r");
+    }
+
+    // Send AT command to request RPM
+    public void requestRPM() throws IOException {
+        sendATCommand("010C\r");
+    }
+
+    // Send an AT command to the device
+    private void sendATCommand(String command) throws IOException {
+        outputStream.write(command.getBytes());
+    }
+
+    // Read the response from the OBD-II device
+    public String readResponse() throws IOException {
+        byte[] buffer = new byte[1024];
+        int bytes = inputStream.read(buffer);
+        return new String(buffer, 0, bytes);
+    }
+
+    // Process the response to extract RPM
+    public int processRPMResponse(String response) {
+        if (response.contains("41 0C")) {
+            try {
+                // Extract hex values after '41 0C'
+                String hexA = response.substring(6, 8);
+                String hexB = response.substring(9, 11);
+
+                // Convert hex to integers
+                int A = Integer.parseInt(hexA, 16);
+                int B = Integer.parseInt(hexB, 16);
+
+                // Calculate RPM
+                return ((A * 256) + B) / 4;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return -1; // Return -1 on error
+            }
+        }
+        return -1; // Return -1 if '41 0C' not found
+    }
+
+    // Close the Bluetooth connection
+    public void disconnect() throws IOException {
+        inputStream.close();
+        outputStream.close();
+        socket.close();
     }
 
 }
